0d13d87d0267edc37959158de1b929e52a30be75
Mateusz Bodziony <matus3000@gmail.com>
2024-04-26 00:33:36 +0200

    Little change

--- kernel/bpf/syscall.c
+++ kernel/bpf/syscall.c
@@ -3730,6 +3730,7 @@ static int bpf_raw_tp_link_attach(struct bpf_prog *prog,
 	pr_info("bpf_raw_tp_link_attach - MB - bpf_link_prime success");
 	
 	err = bpf_probe_register(link->btp, prog);
+	pr_info("bpf_raw_tp_link_attach - MB - bpf_probe_register");
 	if (err) {
 		pr_info("bpf_raw_tp_link_attach - MB - bpf_get_raw_tracepoint fail");
 		bpf_link_cleanup(&link_primer);
